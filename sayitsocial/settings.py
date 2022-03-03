f56930166b64db94d4f9635a47c117a3c3935bc9
Karol Bautrel <karol.bautrel@gmail.com>
2022-03-03 09:20:46 +0100

    Started frontent project with react

--- sayitsocial/settings.py
+++ sayitsocial/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -58,7 +58,10 @@ ROOT_URLCONF = 'sayitsocial.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+
+            os.path.join(BASE_DIR, 'frontend/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,6 +136,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS =[
+
+        os.path.join(BASE_DIR, 'frontend/build/static')
+
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- sayitsocial/urls.py
+++ sayitsocial/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path,include
-
+from django.views.generic import TemplateView
 urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('api/', include('base.urls')),
     path('api/', include('djoser.urls')),
     path('api/', include('djoser.urls.authtoken')),
+    path('', TemplateView.as_view(template_name = 'index.html'))
 ]
